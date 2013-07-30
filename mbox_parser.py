e00a676d1e6260d225956af7f060b5aef93607be
gitabites <clairew@ripariandata.com>
2013-07-30 10:09:58 -0400

    revised scripts

--- mbox_parser.py
+++ mbox_parser.py
@@ -1,6 +1,6 @@
 import mailbox
 import csv
 
-writer = csv.writer(open("cleanmail.csv", "wb"))
+writer = csv.writer(open("clean_mail.csv", "wb"))
 for message in mailbox.mbox('your_mbox_name'):
 	writer.writerow([message['subject'], message['from'], message['date']])
